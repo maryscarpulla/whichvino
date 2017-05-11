4e83ae2c6a2301e0821b4076fbdfb97a709f9fd0
Raghu Betina <raghu@firstdraft.com>
2017-05-11 13:53:12 +0000

    Direct association between users and suggested_wines

--- app/models/suggested_wine.rb
+++ app/models/suggested_wine.rb
@@ -1,6 +1,9 @@
 class SuggestedWine < ApplicationRecord
   # Direct associations
 
+  belongs_to :user_id_to,
+             :class_name => "User"
+
   belongs_to :wine
 
   # Indirect associations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :wine_suggestions_from_friends,
+             :class_name => "SuggestedWine",
+             :foreign_key => "user_id_to",
+             :dependent => :destroy
+
   has_many   :my_wines,
              :dependent => :nullify
 
