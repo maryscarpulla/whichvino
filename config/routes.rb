2459ff25325ae41c21e53edda5083d33c52903ef
Raghu Betina <raghu@firstdraft.com>
2017-05-11 13:53:46 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "my_wines#index"
   # Routes for the Wine_tag_pair resource:
   # CREATE
