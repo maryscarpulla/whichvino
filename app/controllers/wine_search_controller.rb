30e4dd39ccbc389fce232fdfc37c2a42601aff8b
Mary Lin <marylin@chicagobooth.edu>
2017-05-11 12:13:31 -0500

    testing wine search function

---
+++ app/controllers/wine_search_controller.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+
+class WineSearchController < ApplicationController
+  def wine_search_form
+    # Nothing to do here.
+    render("wine_search_form.html.erb")
+  end
+
+  def search_to_results
+    @wine_search = params[:user_wine_search]
+
+    # ==========================================================================
+    # Your code goes below.
+    #
+    # The street address that the user typed is in the variable @street_address.
+    # ==========================================================================
+
+    url = "http://api.snooth.com/wines/?akey=4ki3im2n32x688cqwnb8onkpuwmvxo9yi3pbz4qrtmd9nmwt&ip=66.28.234.115&q=#{@wine_search}&xp=30"
+
+    parsed_data = JSON.parse(open(url).read)
+
+    @winery1 = parsed_data["wines"][0]["winery"]
+    @winery2 = parsed_data["wines"][1]["winery"]
+
+    render("wine_search_results.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,13 @@ Rails.application.routes.draw do
   # DELETE
   get "/delete_wine_tag_pair/:id", :controller => "wine_tag_pairs", :action => "destroy"
   #------------------------------
+##### TEST########
+get "/winesearch/new",:controller => "wine_search_controller", :action => "wine_search_form"
+get "/searchresults/:id",:controller => "wine_search_controller", :action => "search_to_results"
+
+
+
+
 
   # Routes for the Varietal resource:
   # CREATE
