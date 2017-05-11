9d25f337f332e2d4a6f4b949df947803be2b695b
Raghu Betina <raghu@firstdraft.com>
2017-05-11 13:54:01 +0000

    Whitelist columns in SuggestedWine for ActiveAdmin

--- app/admin/suggested_wine.rb
+++ app/admin/suggested_wine.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SuggestedWine do
 
+ permit_params :wine_id, :user_id_from, :user_id_to
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
