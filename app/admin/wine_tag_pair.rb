53e0dbd7ce7b9e2dd70a6a31aaadbf58011416b1
Raghu Betina <raghu@firstdraft.com>
2017-05-11 13:54:18 +0000

    Whitelist columns in WineTagPair for ActiveAdmin

--- app/admin/wine_tag_pair.rb
+++ app/admin/wine_tag_pair.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register WineTagPair do
 
+ permit_params :wine_id, :tag_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
