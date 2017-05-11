ff555a9b0838b199b8d5bb0c1216b5983551629d
Raghu Betina <raghu@firstdraft.com>
2017-05-11 13:54:14 +0000

    Whitelist columns in Varietal for ActiveAdmin

--- app/admin/varietal.rb
+++ app/admin/varietal.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Varietal do
 
+ permit_params :varietal_name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
